722aa667a773a9afbf06ecbb69d5206bc1855701
Russell Best <ellrussbest@gmail.com>
2022-11-08 09:07:20 +0300

    changes

--- src/context/index.js
+++ src/context/index.js
@@ -29,7 +29,7 @@ export default function UserProvider({ children }) {
       baseURL: baseURL,
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJKMzFzLzEyMDY4LzIwMTgiLCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2Njc4NDc0NzUsImV4cCI6MTY2NzkzMzg3NX0.zzePriq-c6fNeUHf862PEi_C3w4DovzfITCDnjsnags`,
+        Authorization: `Bearer ${token}`,
       },
     });
   }, [token]);
